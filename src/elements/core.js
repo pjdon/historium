f047d465bed8c26e7d1a0a610c899afa87072e46
pjd <rovyko@protonmail.com>
2019-02-19 00:09:12 -0500

    added history-group custom element

--- src/elements/core.js
+++ src/elements/core.js
@@ -49,3 +49,65 @@
   });
 
 })();
+
+(function () {
+  const template = document.createElement('template');
+  template.innerHTML = `
+    <link rel='stylesheet' href='elements/group.css' />
+    <div id='group'>
+      <div id='header'>
+        <span id='title'>Sunday, February 10, 2018</span>
+      </div>
+      <div id='list'>
+      </div>
+    </div>
+  `;
+
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  customElements.define('h-group', class extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({
+        mode: 'open'
+      });
+      this.shadowRoot.appendChild(
+        template.content.cloneNode(true)
+      );
+    }
+
+    headerNameFromDate(date) {
+      date = new Date(date);
+      return `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+    }
+
+    connectedCallback() {
+      this.count;
+
+      if (this.items) {
+        this.count = this.items.length;
+
+        this.list = this.shadowRoot.getElementById('list');
+        this.catalog = [];
+        this.items.forEach( config => {
+          const element = document.createElement('h-item');
+          element.config = config;
+
+          this.list.appendChild(element);
+          this.catalog.push(element);
+
+        });
+
+        if (this.count > 0) {
+          this.shadowRoot.getElementById('title').textContent = this.headerNameFromDate(this.items[0].time);
+        }
+
+
+      }
+
+
+    }
+  });
+})();
