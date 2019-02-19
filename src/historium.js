16586312d891f8047fad7399fa61a456c09fe4be
pjd <rovyko@protonmail.com>
2019-02-19 00:11:34 -0500

    temporary inclusion of group in base app js

--- src/historium.js
+++ src/historium.js
@@ -1,7 +1,16 @@
-const list = document.getElementsByClassName('group')[0];
+const list = document.getElementById('history');
 
-for (let i=0; i<10; i++) {
-  const item = document.createElement('h-item');
-  item.config = data[i];
-  list.appendChild(item);
-}
+// for (let i=0; i<10; i++) {
+//   const item = document.createElement('h-item');
+//   item.config = data[i];
+//   list.appendChild(item);
+// }
+
+let group = document.createElement('h-group')
+group.items = data.slice(0,10);
+list.appendChild(group);
+group = document.createElement('h-group')
+group.items = data.slice(10,20);
+list.appendChild(group);
+
+const history = new HistoryApi();
