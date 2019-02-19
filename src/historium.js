352c2fe7265829b64b9f6101f54cfd5959094d6c
pjd <rovyko@protonmail.com>
2019-02-19 15:55:23 -0500

    temporary setup for list api in main js

--- src/historium.js
+++ src/historium.js
@@ -1,16 +1,7 @@
-const list = document.getElementById('history');
-
-// for (let i=0; i<10; i++) {
-//   const item = document.createElement('h-item');
-//   item.config = data[i];
-//   list.appendChild(item);
-// }
-
-let group = document.createElement('h-group')
-group.items = data.slice(0,10);
-list.appendChild(group);
-group = document.createElement('h-group')
-group.items = data.slice(10,20);
-list.appendChild(group);
+const target = document.getElementById('history');
 
 const history = new HistoryApi();
+const list = new ListApi(target, history);
+
+list.addTemp();
+
