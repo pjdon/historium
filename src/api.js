baaa63dc900a5d00f283414058f5ae828f89a8ff
pjd <rovyko@protonmail.com>
2019-02-19 00:12:52 -0500

    create History api for getting visit data
    get objects representing visits grouped by day

---
+++ src/api.js
@@ -0,0 +1,107 @@
+'use strict';
+
+function HistoryApi() {
+
+  /* Currently no good method to stream history items by time period
+  so this is the largest number (2^52) of results that the search will accept
+  TODO: replace with a recursive search function
+  */
+  const maxSearchResults = Math.pow(2, 52);
+  const msPerDay = 86400000;
+  const domainFromURL = new RegExp(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im);
+
+  function getDateStartMs(date) {
+    return (new Date(date.getFullYear(), date.getMonth(), date.getDate())).getTime();
+  }
+
+  function dfcToMs(dfc = 0, end = false, base) {
+    /* Days-from-current-day to milliseconds since epoch */
+    if (!(base instanceof Number)) base = new Date();
+    const start = getDateStartMs(new Date()) - (msPerDay * dfc);
+    if (end) {
+      return start + msPerDay - 1;
+    } else {
+      return start;
+    }
+  }
+
+  function urlToDomain(url) {
+    const result = url.match(domainFromURL);
+    if (result instanceof Array) {
+      return result[1];
+    }
+  }
+
+  function sortByDateDesc(a, b) {
+    return b.datetime - a.datetime;
+  }
+
+  this.searchVisits = async function (text, startTime, endTime, sortFunction = sortByDateDesc) {
+    /* Returns array of visit objects { url, title, domain, time } */
+    const query = {
+      text, startTime, endTime,
+      maxResults: maxSearchResults
+    }
+
+    const results = [];
+    for (let item of (await browser.history.search(query))) {
+
+      const url = item.url;
+      const title = item.title || item.url;
+      const domain = urlToDomain(item.url);
+
+
+      if (item.visitCount == 1) {
+        results.push(
+          { url, title, domain, datetime: new Date(item.lastVisitTime) }
+        );
+      }
+      else if (item.visitCount > 1) {
+        results.push(...(
+          /* Find all visits for the `result` within the time range and save them to `results` as visit objects */
+          (await browser.history.getVisits({ url: item.url }))
+            .filter(visit => {
+              return visit.visitTime >= startTime && visit.visitTime <= endTime;
+            }
+            )
+            .map(visit =>
+              ({ url, title, domain, datetime: new Date(visit.visitTime) })
+            )
+        ));
+      }
+    }
+    if (sortFunction) results.sort(sortFunction);
+    return results;
+  }
+
+  function groupVisits(visits) {
+    console.log(visits);
+    const groups = [];
+    let prevDay = -1;
+    let group;
+    visits.forEach(visit => {
+      const date = visit.datetime = new Date(visit.datetime);
+      const day = date.getDate();
+      console.log(prevDay, day, date);
+
+      if (day != prevDay) {
+        if (group) groups.push(group);
+        group = [visit];
+        prevDay = day;
+      } else {
+        group.push(visit);
+      }
+    });
+    groups.push(group);
+    return groups;
+  }
+
+  this.visitsByDay = async function (text = '', dfcStart = 1, dfcEnd = 0) {
+    const startTime = dfcToMs(dfcStart, false);
+    const endTime = dfcToMs(dfcEnd, true);
+
+    const visits = await this.searchVisits(text, startTime, endTime, false);
+    visits.sort(sortByDateDesc);
+    return groupVisits(visits);
+  }
+}
