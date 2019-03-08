26b7c4e0281188bb1ed76dc28b9c55f11e86a4f1
pjd <rovyko@protonmail.com>
2019-03-07 23:57:36 -0500

    add temp rate limiting decorator

--- src/historium.js
+++ src/historium.js
@@ -3,6 +3,22 @@ const finder = new HistoryVisitFinder();
 const streamer = new HistoryVisitStreamer();
 const display = new HistoryCatalog(table);
 
+const RateLimited = function (callback, msRateLimit, limitedCallback, startLimited = false) {
+  let limited = startLimited;
+  const reset = () => limited = false;
+  return function () {
+    if (!limited) {
+      limited = true;
+      setTimeout(reset, msRateLimit);
+      return callback();
+    }
+    else if (typeof limitedCallback === "function") {
+      return limitedCallback();
+    }
+  }
+}
+
+const bufferDist = 200;
 function addBlock(historyStreamer, historyDisplay) {
   historyStreamer.getNext().then(visits => {
     visits.forEach(row => historyDisplay.addRow(row));
